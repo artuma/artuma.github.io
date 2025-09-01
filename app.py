4b75f52f03ae558d544f9e96dbde7dc7339ab39c
artuma <akbalinumar@mail.ru>
2025-09-01 16:53:52 +0500

    start more site

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST": # если пользователь ввел имя и нажал кнопку отправить
+        name = request.form["username"] # возьмем текст из формы который он отправил
+        return f"Привет {name}!"
+    
+    # иначе покажем ему форму
+    return """
+        <form action="/" method="post">
+            <p>
+	            <label for="username">Username</label>
+	            <input type="text" name="username">
+	        </p>
+	    	<p>
+	            <input type="submit" value="Отправить">
+	        </p>
+        </form>
+        """
+
 
 if __name__ == '__main__':
      app.run(debug=True)
